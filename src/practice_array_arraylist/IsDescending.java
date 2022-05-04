1b787fe2ba13764e370bfd2856206ceeccb58f9b
Veronica Pak <nikampak@gmail.com>
2022-05-04 10:55:20 -0400

    First commit of all files

---
+++ src/practice_array_arraylist/IsDescending.java
@@ -0,0 +1,33 @@
+package practice_array_arraylist;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class IsDescending {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 4, 3, 2, 1};
+        System.out.println(isDesc(arr));
+    }
+
+    public static boolean isDesc(int[] arr) {
+
+        int[] arrToCompare = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(arrToCompare);
+
+        int[] arrToAdd = new int[arrToCompare.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            arrToAdd[arrToCompare.length - 1 - i] = arrToCompare[i];
+        }
+        return (Arrays.equals(arr, arrToAdd));
+
+    }
+}
+
+    //11  if desc  true
+    //  int[]a={7,6,5,4};
+    // check if array's elements are in descending order
+
