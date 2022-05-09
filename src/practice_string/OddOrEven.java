df1251dd04e25a77c0d47ae153417edd00df14fc
Veronica Pak <nikampak@gmail.com>
2022-05-09 11:12:13 -0400

    another practice task

---
+++ src/practice_string/OddOrEven.java
@@ -0,0 +1,18 @@
+package practice_string;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        System.out.println(oddOrEven(0));
+    }
+
+    public static String oddOrEven(int a) {
+        return (a % 2 == 0 ? "Even" : "Odd");
+    }
+}
+/*
+14 Use String
+int a=5;
+
+if number is odd output will be "odd", if number is even output will be "even"
+ */
