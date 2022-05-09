0c319b0b1d6d476cc8f2fdf6e05524ffbdf7cc15
Veronica Pak <nikampak@gmail.com>
2022-05-09 11:20:11 -0400

    another practice task

--- src/practice_string_and_primitives/OddOrEven.java
+++ src/practice_string_and_primitives/OddOrEven.java
@@ -1,4 +1,4 @@
-package practice_string;
+package practice_string_and_primitives;
 
 public class OddOrEven {
 
---
+++ src/practice_string_and_primitives/Remainder.java
@@ -0,0 +1,14 @@
+package practice_string_and_primitives;
+
+public class Remainder {
+
+    public static void main(String[] args) {
+        System.out.println(remainder(5,2));
+    }
+    public static String remainder(int a, int b) {
+        return (a + " divided by " + b + " equals to " + a / b + " and the remainder is " + a % b);
+    }
+
+
+
+}
