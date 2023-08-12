0398afcd9865a77cffe715d5239d54d5785d9b21
pakalexey <mye604@gmail.com>
2023-08-12 12:42:27 -0400

    Lambda Expressions part 2 (day 3) more classes from live

--- src/aJavaDevCourse/day03_lambdaP2/review/CarTest.java
+++ src/aJavaDevCourse/day03_lambdaP2/review/CarTest.java
@@ -1,9 +1,67 @@
 package aJavaDevCourse.day03_lambdaP2.review;
 
+import day16_review.CarpetShop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarTest {
     public static void main(String[] args) {
 
         Car car = Car.builder().make("Toyota").topSpeed(145).year(2000).build();
         System.out.println(car);
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Toyota", 140, 1998));
+        carList.add(new Car("Audi", 200, 2008));
+        carList.add(new Car("BMW", 210, 2020));
+        carList.add(new Car("Honda", 140, 2018));
+
+        System.out.println("Fast Cars");
+//        System.out.println(filterFastCars(carList));
+        System.out.println(filterCars(carList, new CarFastPredicate()));
+
+        System.out.println("New Cars");
+//        System.out.println(filterNewCars(carList));
+        System.out.println(filterCars(carList, new CarNewPredicate()));
+
+        CarPredicate carPredicate = (Car car) -> true;
     }
+
+    //Using two methods to create two very similar methods
+//    private static List<Car> filterFastCars(List<Car> cars){
+//        List<Car> filteredCars = new ArrayList<>();
+//        for(Car car: cars){
+//            if (car.getTopSpeed()>160){
+//                filteredCars.add(car);
+//            }
+//        }
+//        return filteredCars;
+//    }
+//
+//    private static List<Car> filterNewCars(List<Car> cars){
+//        List<Car> filteredCars = new ArrayList<>();
+//        for(Car car: cars){
+//            if (car.getYear()>2015){
+//                filteredCars.add(car);
+//            }
+//        }
+//        return filteredCars;
+//    }
+
+
+    //Using functional interface approach:
+//    private static List<Car> filterCars(List<Car> cars, CarPredicate carPredicate){
+//        List<Car> filteredCars = new ArrayList<>();
+//        for(Car car: cars){
+//            if (carPredicate.test(car)){
+//                filteredCars.add(car);
+//            }
+//        }
+//        return filteredCars;
+//    }
+
+    //Using Lambda
+
+
 }
