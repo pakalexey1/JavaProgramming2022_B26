a238bbdee8a69797222cdd062cd8193f2c8b1dc3
pakalexey <mye604@gmail.com>
2023-08-17 07:47:15 -0400

    Lambda Expressions part 2 (day 3) more classes from live

--- src/aJavaDevCourse/day03_lambdaP2/functionalInterfaceAgain/Examples.java
+++ src/aJavaDevCourse/day03_lambdaP2/functionalInterfaceAgain/Examples.java
@@ -1,5 +1,7 @@
 package aJavaDevCourse.day03_lambdaP2.functionalInterfaceAgain;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Examples {
@@ -24,5 +26,13 @@ public class Examples {
         Boolean result = lesserThan.test(50);
         System.out.println(result);
 
+        //*************Consumer*****************//
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(50);
+
+
+        //*************BiConsumer*****************//
+        BiConsumer<Integer,Integer> addTwo = (x,y)-> System.out.println(x+y);
+        addTwo.accept(100,200);
     }
 }
